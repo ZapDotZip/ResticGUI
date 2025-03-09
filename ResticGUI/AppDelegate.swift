1a6565aeea21dfc803f67fcc11dc3466c2067aac
zap.zip <zapdotzip@gmail.com>
2025-03-08 19:19:09 -0700

    added savePanel functions

--- ResticGUI/AppDelegate.swift
+++ ResticGUI/AppDelegate.swift
@@ -86,6 +86,10 @@ func Alert(title: String, message: String, style: NSAlert.Style, buttons: [Strin
 	return alert.runModal()
 }
 
+
+/// Creates an Open/Save Dialogue panel for the user.
+/// - Parameters:
+/// - Returns: The panel itself, to get URLs from, and the response from the user.
 func openPanel(message: String, prompt: String, canChooseDirectories: Bool, canChooseFiles: Bool, allowsMultipleSelection: Bool, canCreateDirectories: Bool) -> (NSOpenPanel, NSApplication.ModalResponse) {
 	let openPanel = NSOpenPanel()
 	openPanel.message = message
@@ -95,4 +99,37 @@ func openPanel(message: String, prompt: String, canChooseDirectories: Bool, canC
 	openPanel.allowsMultipleSelection = allowsMultipleSelection
 	openPanel.canCreateDirectories = canCreateDirectories
 	return (openPanel, openPanel.runModal())
+}
+
+func savePanel(title: String, message: String, nameFieldLabel: String, nameField: String, currentDirectory: URL?, canCreateDirectories: Bool, canSelectHiddenExtension: Bool) -> (URL?, NSApplication.ModalResponse) {
+	let panel = NSSavePanel()
+	panel.title = title
+	panel.message = message
+	panel.nameFieldLabel = nameFieldLabel
+	panel.nameFieldStringValue = nameField
+	if currentDirectory != nil {
+		panel.directoryURL = currentDirectory
+	}
+	panel.canCreateDirectories = canCreateDirectories
+	panel.canSelectHiddenExtension = canSelectHiddenExtension
+	
+	let res = panel.runModal()
+	return (panel.url, res)
+}
+
+func savePanel(for window: NSWindow, message: String, nameFieldLabel: String, nameField: String, currentDirectory: URL?, canCreateDirectories: Bool, canSelectHiddenExtension: Bool, isExtensionHidden: Bool, completionHandler handler: @escaping (NSApplication.ModalResponse, URL?) -> Void) {
+	let panel = NSSavePanel()
+	panel.message = message
+	panel.nameFieldLabel = nameFieldLabel
+	panel.nameFieldStringValue = nameField
+	if currentDirectory != nil {
+		panel.directoryURL = currentDirectory
+	}
+	panel.canCreateDirectories = canCreateDirectories
+	panel.canSelectHiddenExtension = canSelectHiddenExtension
+	panel.isExtensionHidden = isExtensionHidden
+	
+	panel.beginSheetModal(for: window, completionHandler: { response in
+		handler(response, panel.url)
+	})
 }
