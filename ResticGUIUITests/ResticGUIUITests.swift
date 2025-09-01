5a9c0394907f4df689821dafb4144cebcb5f59ea
zap.zip <zapdotzip@gmail.com>
2025-09-01 16:56:06 -0700

    add some basic UI tests

--- ResticGUIUITests/ResticGUIUITests.swift
+++ ResticGUIUITests/ResticGUIUITests.swift
@@ -6,35 +6,62 @@
 import XCTest
 
 class ResticGUIUITests: XCTestCase {
+	
+	var app: XCUIApplication! = nil
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+	override func setUp() {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+		app = XCUIApplication()
+		app.launchArguments = ["--test"]
+		app.activate()
+		app.cells.containing(.staticText, identifier: "ProfilesHeader").firstMatch.typeText("testingprofile\r")
+		
+		continueAfterFailure = false
+	}
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+	
+	
+	func testAddAndDeleteProfile() {
+		app.buttons["addProfileButton"].click()
+		app.cells.containing(.staticText, identifier: "ProfilesHeader").firstMatch.typeText("test\r")
+		XCTAssert(app.cells.textFields["test"].exists)
+		app.cells.textFields["test"].click()
+		app.buttons["deleteProfileButton"].click()
+		app.dialogs.buttons["Delete"].click()
+		XCTAssertFalse(app.cells.textFields["test"].exists)
+	}
+	
+	func testAddAndRemoveRepo() {
+		app.groups.buttons["add"].click()
+		let element = app.cells.containing(.staticText, identifier: "ProfilesHeader").firstMatch
+		element.typeText("test repo")
+		app.textFields["repoPath"].click()
+		element.typeText("/test")
+		app.secureTextFields["repoPassword"].click()
+		element.typeText("password")
+		app.sheets.buttons["repoSaveEditButton"].click()
+		
+		app.popUpButtons["repoPopUpList"].click()
+		XCTAssert(app.menuItems["test repo"].exists)
+		app.popUpButtons["repoPopUpList"].click()
+		
+		app.groups.buttons["remove"].click()
+		app.dialogs.buttons["Delete"].click()
+		
+		app.popUpButtons["repoPopUpList"].click()
+		XCTAssertFalse(app.menuItems["test repo"].exists)
+	}
+	
+	
+	func testLaunchPerformance() {
+		if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+			// This measures how long it takes to launch your application.
+			measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+				XCUIApplication().launch()
+			}
+		}
+	}
 }
