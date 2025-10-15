571dc2935f586afa6e18742678d6c3b834d89c52
zap.zip <zapdotzip@gmail.com>
2025-10-15 08:44:28 -0700

    remember which preference tab was last selected

--- ResticGUI/Preferences/PreferencesController.swift
+++ ResticGUI/Preferences/PreferencesController.swift
@@ -14,7 +14,18 @@ class PreferencesTabController: NSTabViewController {
 	
 	override func viewDidAppear() {
 		super.viewDidAppear()
+		selectedTabViewItemIndex = UserDefaults.standard.integer(forKey: "PrefWindowLastSelectedTab")
 		self.view.window!.title = "Preferences"
+	}
+	
+	var calls = 0
+	override func tabView(_ tabView: NSTabView, didSelect tabViewItem: NSTabViewItem?) {
+		super.tabView(tabView, didSelect: tabViewItem)
+		if calls > 1 {
+			UserDefaults.standard.set(selectedTabViewItemIndex, forKey: "PrefWindowLastSelectedTab")
+		} else {
+			calls += 1
+		}
 	}
 	
 }
