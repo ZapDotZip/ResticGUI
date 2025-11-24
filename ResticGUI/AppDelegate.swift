b3c19f951e3dcbd07c3a1682202bbcfcf4647491
zap.zip <zapdotzip@gmail.com>
2025-11-24 14:57:22 -0700

    fix issue where force unlocking would always run after the alert

--- ResticGUI/AppDelegate.swift
+++ ResticGUI/AppDelegate.swift
@@ -166,7 +166,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	
 	@IBAction func menuRepositoryUnlockPressed(_ sender: NSMenuItem) {
 		guard let selectedRepo = ReposManager.default.getSelectedRepo() else { return }
-		STBAlerts.destructiveAlert(title: "Unlock Repository?", message: "Are you sure you want to unlock the repository \"\(selectedRepo.getName())\"? This may result in corruption if another process is using this repository.", style: .warning, destructiveButtonText: "Force Unlock")
+		guard STBAlerts.destructiveAlert(title: "Unlock Repository?", message: "Are you sure you want to unlock the repository \"\(selectedRepo.getName())\"? This may result in corruption if another process is using this repository.", style: .warning, destructiveButtonText: "Force Unlock") else { return }
 		do {
 			try ResticController.default.unlockRepository(selectedRepo)
 			STBAlerts.alert(title: "Repository unlocked", message: "The repository has been unlocked.", style: .informational)
